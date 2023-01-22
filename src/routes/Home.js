b98fc10ef03b897ba7e07a89ec2a7dcfd0e69d92
nayeong <nyk1118@naver.com>
2023-01-22 17:05:33 +0900

    nweets list

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,22 @@
 import { dbService } from "fbase";
-import { useState } from "react";
-import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+// import { Navigate } from "react-router-dom";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+
+    const getNweets = async () => {
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach((document) => {
+            const nweetObject = { ...document.data(), id: document.id };
+            setNweets((prev) => [nweetObject, ...prev])
+    });
+    };
+
+    useEffect(() => {
+        getNweets();
+    }, []);
 
     const onSubmit = async (event) => {
         event.preventDefault();
@@ -23,6 +36,7 @@ const Home = () => {
     };
 
     return (
+        <>
         <form onSubmit={onSubmit}>
             <input
                 value={nweet}
@@ -33,6 +47,14 @@ const Home = () => {
             />
             <input type="submit" value="Nweet" />
         </form>
+        <div>
+            {nweets.map((nweet) => (
+                <div key={nweet.id}>
+                    <h4>{nweet.text}</h4>
+                </div>
+            ))}
+        </div>
+        </>
     );
 };
 
